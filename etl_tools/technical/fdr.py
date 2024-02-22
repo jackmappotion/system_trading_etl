46caa87bb494dbedb508e30bae8ad4d259a75b5d
osilixir <osilixir@gmail.com>
2024-02-22 16:55:08 +0900

    :heavy_plus_sign: [add] typehints

--- etl_tools/technical/fdr.py
+++ etl_tools/technical/fdr.py
@@ -1,13 +1,16 @@
+import datetime as dt
 import pandas as pd
 from tqdm import tqdm
 import FinanceDataReader as fdr
+
+date = dt.date | str
 
 
 class FdrExtractor:
     def __init__(self) -> None:
         self.fdr = fdr
 
-    def load_stocks(self, only_major=True):
+    def load_stocks(self, only_major: bool = True) -> pd.DataFrame:
         """
         only_major : [KOSPI / KOSDAQ]
         """
@@ -16,12 +19,12 @@ class FdrExtractor:
             stocks = stocks[stocks["Market"].isin(["KOSPI", "KOSDAQ"])]
         return stocks
 
-    def load_ohlcv(self, stock_code, start_date, end_date):
+    def load_ohlcv(self, stock_code: str, start_date: date, end_date: date) -> pd.DataFrame:
         ohlcv = self.fdr.DataReader(symbol=stock_code, start=start_date, end=end_date)
         ohlcv["stock_code"] = stock_code
         return ohlcv
 
-    def load_ohlcvs(self, stock_codes, start_date, end_date):
+    def load_ohlcvs(self, stock_codes: list, start_date: date, end_date: date) -> pd.DataFrame:
         ohlcv_list = list()
         for stock_code in tqdm(stock_codes):
             try:
@@ -29,5 +32,5 @@ class FdrExtractor:
                 ohlcv_list.append(ohlcv)
             except:
                 print(stock_code)
-        ohlcvs = pd.concat(ohlcv_list,axis=0)
+        ohlcvs = pd.concat(ohlcv_list, axis=0)
         return ohlcvs
